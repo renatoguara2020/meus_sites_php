88a40a290a6dc96dc9405fe0582a195ba55e90df
Renato Alves Soares <renatoguara2020@gmail.com>
2022-12-28 08:39:27 -0300

    ok

---
+++ soure_code_updated/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title>Homepage</title>
+</head>
+<body>
+<h2>User Authentication System </h2><hr>
+
+<P>You are currently not signin <a href="login.php">Login</a> Not yet a member? <a href="signup.php">Signup</a> </P>
+
+<p>You are logged in as {username} <a href="logout.php">Logout</a> </p>
+</body>
+</html>
